80c5f01e375fbe6985167b1d1cfc22029eab8aec
Tommy Egbe <tabe_e@yahoo.ca>
2022-10-07 13:36:43 +0200

    edited app component and user registration component

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -32,10 +32,12 @@ registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
+     console.log(result)
      this.snackBar.open(result, 'OK', {
         duration: 2000
      });
     }, (result) => {
+      console.log(result)
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
