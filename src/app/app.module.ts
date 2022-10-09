3a371f12dd270806e0a828e70f77a366f3cfdc04
Tommy Egbe <tabe_e@yahoo.ca>
2022-10-10 01:16:16 +0200

    added genre component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
+import { GenreComponent } from './genre/genre.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -35,6 +36,7 @@ const appRoutes: Routes = [
     LoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
+    GenreComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
+import { GenreComponent } from '../genre/genre.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +12,11 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 export class MovieCardComponent implements OnInit {
 
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService ) { }
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+     ) { }
 
 
   ngOnInit(): void {
@@ -19,9 +26,18 @@ export class MovieCardComponent implements OnInit {
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
         this.movies = resp;
-        console.log(this.movies);
         return this.movies;
       });
+    }
+
+    openGenreDialog(name:string, description:string): void{
+      this.dialog.open(GenreComponent,{
+        data: {
+          Name: name,
+          Description: description,
+        },
+        width: '500px'
+      })
     }
 
 }
